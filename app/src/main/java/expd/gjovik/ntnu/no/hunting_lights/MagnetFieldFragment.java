8e40e35226dba25535cd01e6734d14e8c1eb3519
antizwiebel <Penedermark@gmail.com>
2017-11-12 22:06:14 +0100

    - add route with mock directions to RouteFragment   - add MapView to magnetFields, image overlay still not working

--- app/src/main/java/expd/gjovik/ntnu/no/hunting_lights/MagnetFieldFragment.java
+++ app/src/main/java/expd/gjovik/ntnu/no/hunting_lights/MagnetFieldFragment.java
@@ -1,12 +1,34 @@
 package expd.gjovik.ntnu.no.hunting_lights;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.nio.ByteBuffer;
 
 
 /**
@@ -14,7 +36,18 @@ import android.view.ViewGroup;
  * Use the {@link MagnetFieldFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MagnetFieldFragment extends Fragment {
+public class MagnetFieldFragment extends Fragment implements OnMapReadyCallback{
+
+    private MapView mMap;
+    private View mView;
+    private GoogleMap mGoogleMap;
+    private ImageView mImageVIew;
+
+    //TODO: change coordinates
+    private static final LatLng NEWARK = new LatLng(40.714086, -74.228697);
+
+    private static final LatLng NEAR_NEWARK =
+            new LatLng(NEWARK.latitude - 0.001, NEWARK.longitude - 0.025);
 
     public MagnetFieldFragment() {
         // Required empty public constructor
@@ -34,6 +67,8 @@ public class MagnetFieldFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        MapFragment mapFragment = (MapFragment) getActivity().getFragmentManager().findFragmentById(R.id.map);
 
     }
 
@@ -41,7 +76,22 @@ public class MagnetFieldFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_magnet_field, container, false);
+        mView = inflater.inflate(R.layout.fragment_route, container, false);
+        mImageVIew = mView.findViewById(R.id.imageOverlay);
+        mMap = mView.findViewById(R.id.map);
+        mImageVIew.bringToFront();
+
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (mMap != null) {
+            mMap.onCreate(null);
+            mMap.onResume();
+            mMap.getMapAsync(this);
+        }
     }
 
     @Override
@@ -49,4 +99,20 @@ public class MagnetFieldFragment extends Fragment {
         super.onDetach();
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+
+        MapsInitializer.initialize(getContext());
+        mGoogleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        mGoogleMap.moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(69.653333, 18.958087) , 14.0f) );
+        //mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(NEWARK, 11));
+
+        //BitmapDescriptor imageDescriptor = BitmapDescriptorFactory.fromResource(R.drawable.newark_nj_1922);
+        //mGoogleMap.addGroundOverlay(new GroundOverlayOptions()
+        //        .image(imageDescriptor).position(new LatLng(69.653333, 18.958087), 4300f, 3025f)
+        //        .transparency((float) 0.5));
+    }
 }
--- app/src/main/java/expd/gjovik/ntnu/no/hunting_lights/RouteFragment.java
+++ app/src/main/java/expd/gjovik/ntnu/no/hunting_lights/RouteFragment.java
@@ -1,24 +1,37 @@
 package expd.gjovik.ntnu.no.hunting_lights;
 
-import android.content.Context;
-import android.net.Uri;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.TravelMode;
+
+import org.joda.time.DateTime;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -64,13 +77,14 @@ public class RouteFragment extends Fragment implements OnMapReadyCallback {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.fragment_route, container, false);
+        mMap = (MapView) mView.findViewById(R.id.map);
         return mView;
     }
 
     @Override
     public void onViewCreated(View view,  Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mMap = (MapView) mView.findViewById(R.id.map);
+
         if (mMap != null) {
             mMap.onCreate(null);
             mMap.onResume();
@@ -91,13 +105,76 @@ public class RouteFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         MapsInitializer.initialize(getContext());
 
+        LatLng tromsoCoordinates = new LatLng(69.653333, 18.958087);
         mGoogleMap = googleMap;
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(69.653378, 18.928732)).title("Tromso"));
 
-        CameraPosition tromso = CameraPosition.builder().target(new LatLng(69.653378, 18.928732)).zoom(100).build();
-        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(tromso));
+
+        DateTime now = new DateTime();
+        try {
+            DirectionsResult result = DirectionsApi.newRequest(getGeoContext())
+                    .mode(TravelMode.DRIVING).origin(new com.google.maps.model.LatLng(69.653333, 18.958087))
+                    .destination(new com.google.maps.model.LatLng(69.853258, 18.820932)).departureTime(now)
+                    .await();
+            addMarkersToMap(result, mGoogleMap);
+            addPolyline(result, mGoogleMap);
+        } catch (ApiException | InterruptedException | IOException _e) {
+            _e.printStackTrace();
+        }
+    }
+
+    private void fitAllMarkers(final GoogleMap mMap, List<MarkerOptions> markers) {
+
+        /**create for loop for get the latLngbuilder from the marker list*/
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for (MarkerOptions m : markers) {
+            builder.include(m.getPosition());
+        }
+        /**initialize the padding for map boundary*/
+        int padding = 200;
+        /**create the bounds from latlngBuilder to set into map camera*/
+        LatLngBounds bounds = builder.build();
+        /**create the camera with bounds and padding to set into map*/
+        final CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+        /**call the map call back to know map is loaded or not*/
+        mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+            @Override
+            public void onMapLoaded() {
+                /**set animated zoom camera into map*/
+                mMap.moveCamera(cu);
+
+            }
+        });
+
+    }
+
+    private void addPolyline(DirectionsResult results, GoogleMap mMap) {
+        List<LatLng> decodedPath = PolyUtil.decode(results.routes[0].overviewPolyline.getEncodedPath());
+        mMap.addPolyline(new PolylineOptions().addAll(decodedPath).color(Color.RED));
+    }
+
+    private void addMarkersToMap(DirectionsResult results, GoogleMap mMap) {
+        //save to list in order to be able to fit all markers in the mapView
+        List<MarkerOptions> markers = new ArrayList<>();
+        MarkerOptions marker1 = new MarkerOptions().position(new LatLng(results.routes[0].legs[0].startLocation.lat,results.routes[0].legs[0].startLocation.lng)).title(results.routes[0].legs[0].startAddress);
+        MarkerOptions marker2 = new MarkerOptions().position(new LatLng(results.routes[0].legs[0].endLocation.lat,results.routes[0].legs[0].endLocation.lng)).title(results.routes[0].legs[0].endAddress).snippet(getEndLocationTitle(results));
+        markers.add(marker1);
+        markers.add(marker2);
+        mMap.addMarker(marker1);
+        mMap.addMarker(marker2);
+        fitAllMarkers(mMap, markers);
+    }
+
+    private String getEndLocationTitle(DirectionsResult _results) {
+        return "Recommendation";
+    }
+
+    private GeoApiContext getGeoContext() {
+        GeoApiContext geoApiContext = new GeoApiContext();
+        return geoApiContext.setQueryRateLimit(3).setApiKey(getString(R.string.google_maps_key))
+                .setConnectTimeout(10, TimeUnit.SECONDS)
+                .setReadTimeout(10, TimeUnit.SECONDS).setWriteTimeout(1, TimeUnit.SECONDS);
     }
 
     @Override
@@ -105,4 +182,7 @@ public class RouteFragment extends Fragment implements OnMapReadyCallback {
         super.onDetach();
     }
 
+
 }
+
+
