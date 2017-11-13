5e934c290e8561a9a4f2868cb4dbbfb6363c6a76
moly94 <felix.mauler@gmail.com>
2017-11-13 11:38:38 +0100

    not working line Graph

--- app/src/main/java/expd/gjovik/ntnu/no/hunting_lights/WeatherFragment.java
+++ app/src/main/java/expd/gjovik/ntnu/no/hunting_lights/WeatherFragment.java
@@ -1,11 +1,22 @@
 package expd.gjovik.ntnu.no.hunting_lights;
 
 
+import android.graphics.Color;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,7 +24,12 @@ import android.view.ViewGroup;
  * Use the {@link WeatherFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
+
+
 public class WeatherFragment extends Fragment {
+
+
+    View mView;
 
     public WeatherFragment() {
         // Required empty public constructor
@@ -33,14 +49,47 @@ public class WeatherFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
+        mView= inflater.inflate(R.layout.fragment_weather, container, false);
         return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+
+    @Override
+    public void onActivityCreated (Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        populateChart();
+    }
+
+
+    //populate Chart
+    void populateChart(){
+
+        LineChart weatherChart = mView.findViewById(R.id.linechart1);
+
+
+        List<Entry> lineList = new ArrayList<Entry>();
+        lineList.add(new Entry(1f, 0f));
+        lineList.add(new Entry(2f, 1f));
+        lineList.add(new Entry(3f, 2f));
+        lineList.add(new Entry(4f, 3f));
+        lineList.add(new Entry(5f, 4f));
+
+        LineDataSet learnDataSet = new LineDataSet(lineList, "Line");
+        LineData lineData = new LineData(learnDataSet);
+        weatherChart.setData(lineData);
+        weatherChart.invalidate(); // refresh
+        
+
     }
 
 }
