e1b2a1cfc60a9274a89595f972ef3aa4bb2f76aa
moly94 <felix.mauler@gmail.com>
2017-11-13 15:46:51 +0100

    finished temperature lineChart + Styling like in iOS

--- app/src/main/java/expd/gjovik/ntnu/no/hunting_lights/WeatherFragment.java
+++ app/src/main/java/expd/gjovik/ntnu/no/hunting_lights/WeatherFragment.java
@@ -11,13 +11,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -51,7 +55,7 @@ public class WeatherFragment extends Fragment {
         mView = inflater.inflate(R.layout.fragment_weather, container, false);
         // in this example, a LineChart is initialized from xml
         LineChart chart = (LineChart) mView.findViewById(R.id.lineChart);
-        int[] dataObjects = new int[]{-2,0,3,4};
+        int[] dataObjects = new int[]{-2,0,1,5,8,4,11,12};
 
         List<Entry> entries = new ArrayList<Entry>();
 
@@ -60,18 +64,46 @@ public class WeatherFragment extends Fragment {
             entries.add(new Entry(i, dataObjects[i]));
         }
         LineDataSet dataSet = new LineDataSet(entries, "Temperatures in °C"); // add entries to dataset
-        dataSet.setColor(Color.CYAN);
+        dataSet.setColor(Color.WHITE);
         dataSet.setLineWidth(3.0f);
+        //dataSet.setDrawCircleHole(true);
+        dataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        dataSet.setDrawFilled(true);
         LineData lineData = new LineData(dataSet);
         chart.setData(lineData);
-        chart.setGridBackgroundColor(Color.WHITE);
+        //chart.setGridBackgroundColor(Color.CYAN);
+        GradientDrawable drawable = (GradientDrawable) ContextCompat.getDrawable(getContext(), R.drawable.chart_gradient);
+        dataSet.setFillDrawable(drawable);
+
+        final HashMap<Integer, String> numMap = new HashMap<>();
+        numMap.put(0, "00:00");
+        numMap.put(1, "02:00");
+        numMap.put(2, "04:00");
+        numMap.put(3, "06:00");
+        numMap.put(4, "08:00");
+        numMap.put(5, "10:00");
+        numMap.put(6, "12:00");
+        numMap.put(7, "14:00");
+
         XAxis xAxis = chart.getXAxis();
         xAxis.setDrawGridLines(false);
+
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return numMap.get((int)value);
+            }
+        });
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        chart.setBackgroundColor(Color.parseColor("#8ABFC2"));
         chart.setBorderWidth(3.0f);
         chart.animateY(2000);
         Description description = new Description();
         description.setText("Temperatures in °C");
+        description.setTextColor(Color.WHITE);
+        description.setTextSize(25);
         chart.setDescription(description);
+
         chart.invalidate(); // refresh
         return mView;
     }
